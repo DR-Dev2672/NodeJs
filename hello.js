5763b7e5c7ad3be836719ac785df49d01d3d11d5
dev <thakurdev2672@gmail.com>
2024-10-13 23:33:31 +0530

    modular learning

---
+++ hello.js
@@ -0,0 +1,10 @@
+// console.log("welcome in this node ")
+
+const math=require("./math")
+//const {add,sub}=requre("./math")
+//destructuring...
+
+console.log(math.add(2,5)) 
+//console.log(add(2,5)) when use distructuring import thing
+
+console.log(math.sub(2,5)) 
