d5aba1eaa51a39f061adaf59d0bb49602fe3be03
dev <thakurdev2672@gmail.com>
2024-10-16 20:45:16 +0530

    server with express basics

---
+++ Server_with_express/index.js
@@ -0,0 +1,9 @@
+const express=require("express")
+const app=express()
+
+app.get('/',(req,res)=>{
+    res.send("hii this is homepage from welcome through express")
+})
+app.listen(8000,()=>{
+    console.log("server with express is ready")
+})
