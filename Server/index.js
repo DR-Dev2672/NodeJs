fd62f8e71790a319dc4cdf6649195cda32eefd37
dev <thakurdev2672@gmail.com>
2024-10-16 20:21:40 +0530

    url parsing and http methods

--- Server/index.js
+++ Server/index.js
@@ -1,19 +1,30 @@
 const http=require("http")
 const fs=require("fs")
+const url=require("url")
+
 
 
 
 const myServer=http.createServer((req,res)=>{
- const log=(`server is created ${Date.now()} ${req.url}\n`)
+ const log=(`server is created ${Date.now()}: this is   ${req.url}  \n`)
  if(req.url==="/favicon.ico") return;
+ const myUrl=url.parse(req.url,true);
+ console.log(myUrl)
     fs.appendFile("contact.txt",log,(err,data)=>{
      switch(req.url){
         case "/":
+        if(req.method==='GET'){
             res.end(`hii this is homepage`)
+        }
+            
             break
         case "/about":
             res.end(`this is about`)
             break
+        case "/signup":
+            if(req.method==='GET')res.end("hii this is signup")
+            else if(req.method==='POST'){res.end("this is post method call on signup form")}
+
         default:
             res.end("this is 404 not found")
 
