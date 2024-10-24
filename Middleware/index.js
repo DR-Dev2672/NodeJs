0512ec6a297793728a7615379dea6df9bf395129
dev <thakurdev2672@gmail.com>
2024-10-24 23:19:19 +0530

    all basics in middleware

--- Middleware/index.js
+++ Middleware/index.js
@@ -1,8 +1,32 @@
 const express=require('express');
 const app=express();
 
+//    middle ware are of two types
+//    first one is built in and second is 
+//    custom build
+
+//built in
+app.use(express.urlencoded({extended:false}))
+
+
+//custom built
+app.use((req,res,next)=>{
+ console.log("this is Middleware 1")
+ req.myUserName="dev";
+ next();
+ return res.json({msg:"hello from middleware 1"})
+ 
+})
+
+app.use((req,res,next)=>{
+    console.log("this is Middleware 2")
+    console.log(req.myUserName);
+    next()
+   })
+
 app.get('/user',(req,res)=>{
     res.send("welcome to user route")
+   
 })
 
 app.listen(3000,()=>{
